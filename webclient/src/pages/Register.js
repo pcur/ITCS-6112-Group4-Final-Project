d7e73a66eff99a9220a31fb7ca53d9f8e41f618a
pcur <92280141+pcur@users.noreply.github.com>
2025-04-08 18:00:31 -0400

    add register page

--- webclient/src/pages/Register.js
+++ webclient/src/pages/Register.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { registerUser } from '../api/users';
 
 const Register = () => {
   const [username, setUsername] = useState('');
